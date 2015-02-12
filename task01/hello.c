92b9fa73d922aaed2b7be588c87fe48582d4b838
Renan Prata <renancprata@gmail.com>
2015-02-11 22:37:00 -0200

    adding first task

---
+++ task01/hello.c
@@ -0,0 +1,35 @@
+/* Copyright (c) 2015 Renan Prata <renanprata@ieee.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#define KERNEL_MODULE_AUTHOR "Renan Prata"
+#define KERNEL_MODULE_DESC "A simple Linux Kernel Module that when loaded prints to the kernel debug log level: \"Hello World!\""
+
+int init_module(void)
+{
+	printk(KERN_DEBUG "Hello world!\n");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_DEBUG "Goodbye world!\n");
+}
+
+MODULE_LICENSE("GPL");
+
+MODULE_AUTHOR(KERNEL_MODULE_AUTHOR);
+MODULE_DESCRIPTION(KERNEL_MODULE_DESC);
